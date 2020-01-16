a7352cee3107571e48c0e72a1e1521bbadee05e3
netohelvecio <helvecioneto77@gmail.com>
2020-01-15 22:00:46 -0300

    Estilizando edit

--- frontend/src/pages/EditDev/index.js
+++ frontend/src/pages/EditDev/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import AvatarInput from '../../components/AvatarInput';
+
+import { Container, Coordinates } from './styles';
 
 export default function EditDev() {
-  return <h1>EditDev</h1>;
+  return (
+    <Container>
+      <strong>Editar Dev</strong>
+
+      <form>
+        <AvatarInput name="file" />
+
+        <label htmlFor="name">Nome</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="techs">Tecnologias</label>
+        <input type="text" id="techs" name="techs" />
+
+        <label htmlFor="bio">Bio</label>
+        <textarea name="bio" id="bio" cols="20" rows="4" />
+
+        <Coordinates>
+          <div>
+            <label htmlFor="latitude">Latitude</label>
+            <input type="text" id="latitude" name="latitude" />
+          </div>
+
+          <div>
+            <label htmlFor="longitude">Longitude</label>
+            <input type="text" id="longitude" name="longitude" />
+          </div>
+        </Coordinates>
+
+        <button type="submit">Salvar</button>
+      </form>
+
+      <button type="button">Cancelar</button>
+    </Container>
+  );
 }
